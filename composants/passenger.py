eff18a878e61f8762b49ebf591e5a28590f09f7a
Bryan Gabriel-Atangana Mboa <bryan.mboa@gmail.com>
2024-11-26 13:54:19 -0500

    passenger class implementation

--- composants/passenger.py
+++ composants/passenger.py
@@ -1,2 +1,19 @@
 class Passenger:
-    NotImplemented
+    def __init__(self, origin, destination):
+        """
+        Initialize a passenger with a pickup and drop-off location.
+        Args:
+            origin: List [x, y] of pickup location.
+            destination: List [x, y] of drop-off location.
+        """
+        self.origin = origin
+        self.destination = destination
+        self.in_taxi = False
+
+    def pickup(self):
+        """Mark the passenger as picked up."""
+        self.in_taxi = True
+
+    def drop_off(self):
+        """Mark the passenger as dropped off."""
+        self.in_taxi = False
